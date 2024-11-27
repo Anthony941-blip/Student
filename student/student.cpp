431e775cdb39de2c7bb6770285b44b217384325a
Anthony941-blip <2023260043@ub.edu.bz>
2024-11-27 10:16:24 -0600

    Add files via upload

---
+++ student/student.cpp
@@ -0,0 +1,48 @@
+#include "student.h"
+#include <iostream>
+#include <iomanip>
+
+
+Student::Student(string studentName) {
+
+      name = studentName;
+}
+
+
+
+
+void Student::addGrade(double grade) {
+
+      grades.push_back(grade);
+}
+
+
+
+
+double Student::calculateAverage() {
+  
+  if (grades.empty()) {
+      return 0.0;
+  }
+  
+  double sum = 0.0;
+
+  for (double grade : grades) {
+      sum += grade;
+    }
+
+   return sum / grades.size();
+}
+
+
+
+
+
+//the method that prints Student name 
+//and grade that rounds to two d.p.
+
+void Student::printDetails() {
+
+    cout << "Student name: " << name << endl;
+    cout << "Average Grade: " << fixed << setprecision(2) << calculateAverage() << endl;     
+}
